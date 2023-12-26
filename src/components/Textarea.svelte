77cf7c869d1c3decca0d59356b0e5adcf45f7ef8
Andres Obando <practicante@plasticolab.com>
2023-12-26 12:24:35 -0500

    first commit for repo

---
+++ src/components/Textarea.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { isValidInput } from '../services/store'
+    import { chechLanguage } from '../services/ia'
+    import { get } from 'svelte/store';
+    import debounce from 'just-debounce-it'
+
+    const handleChange = debounce(async (e) => {
+        const { value } = e.target
+        const isValid = value.length > 0
+        if(!isValid) { 
+            isValidInput.set(false)
+            return
+        }
+        const isEnglish = await chechLanguage(value)
+        isValidInput.set(isEnglish)
+        console.log(get(isValidInput))
+    }, 500)
+</script>
+
+<textarea on:input={handleChange} 
+    placeholder="" 
+    id="message" 
+    rows="4" 
+    class={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 }`}>    
+</textarea>
---
+++ src/services/ia.js
@@ -0,0 +1,63 @@
+const COHERE_API_KEY = 'a8vaLhXYH0ejqn6MiL5Ws7w0NE46e0wLQlZSrTzd'
+const COHERE_API_URL = 'https://api.cohere.ai/generate'
+const COHERE_API_LANGUAGE_URL = 'https://api.cohere.ai/detect-language'
+
+export async function chechLanguage(input){
+    const data = {
+        texts: [input]
+    }
+
+    const response = await fetch(COHERE_API_LANGUAGE_URL,{
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `BEARER ${COHERE_API_KEY}`,
+            "Content-Type": 'application/json',
+            "Cohere-Version": '2022-12-06'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+
+    const res = response.results[0].language_name === 'English'
+    
+    return res
+}
+
+export async function fixMyEnglish(input){
+
+    const data = {
+        mode: 'xlarge',
+        prompt: `This is a spell checker generator.
+            --
+                Incorrect sample: "I are good!"
+                Correct sample: "I am good!"
+            --
+                Incorrect sample: "I have 22 years old"
+                Correct sample: "I am 22 years old"
+            --
+                Incorrect sample: "I don't can know"
+                Correct sample: "I don't know"
+            --
+                Incorrect sample: "${input}"
+                Correct sample:`,
+        max_tokens: 40,
+        temperature: 0.3,
+        k: 0,
+        p: 1,
+        frequency_penalty: 0,
+        stop_sequences:['--'],
+        return_likelihoods: 'NONE'
+    }
+
+    const response = await fetch(COHERE_API_URL,{
+        method: 'POST',
+        headers: {
+            Authorization: `BEARER ${COHERE_API_KEY}`,
+            "Content-Type": 'application/json',
+            "Cohere-Version": '2022-12-06'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+
+    return response.generations[0].text.split('"')[1]
+}
