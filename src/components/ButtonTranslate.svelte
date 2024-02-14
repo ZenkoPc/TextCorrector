97be070ac65a2c26f2b7041b454765074e1b249b
Andres Obando <pipeobandog@hotmail.com>
2024-02-14 13:22:12 -0500

    second option added, translator ready

--- src/components/ButtonTranslate.svelte
+++ src/components/ButtonTranslate.svelte
@@ -1,16 +1,16 @@
 <script>
-    import { fixMyEnglish } from '../services/ia'
+    import { translate } from '../services/ia'
     import { isValidInput } from '../services/store';
     import debounce from 'just-debounce-it'
 
     let promise = null
 
     const handleClick = debounce(async () => {
-        const text = document.getElementById('message').value
-        promise = fixMyEnglish(text)
+        const text = document.getElementById('messageTranslator').value
+        promise = translate(text)
         const value = await promise
         
-        document.getElementById('messageRes').value = value
+        document.getElementById('messageTranslatorRes').value = value
         promise = null
     }, 500)
 </script>
@@ -18,7 +18,7 @@
 {#if promise === null}
 
 <button disabled={!$isValidInput} on:click={handleClick} type="button" class={`mt-5 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ${!$isValidInput ? "pointer-events-none opacity-30 transition-all" : '' }`}>
-    Please, Fix my English!
+    Please, Translate my English!
 </button>
 
 {:else}
--- src/services/ia.js
+++ src/services/ia.js
@@ -61,3 +61,42 @@ export async function fixMyEnglish(input){
 
     return response.generations[0].text.split('"')[1]
 }
+
+export async function translate(input){
+    const data = {
+        mode: 'xlarge',
+        prompt: `
+            This is a word translator from english to spanish.
+                --
+                    user: "Hi, I am Andres!"
+                    response: "Hola, mi nombre es andres"
+                --
+                    user: "Did you know about the orange?"
+                    response: "Sabes acerca de la naranja?"
+                --
+                    user: "Well played boy"
+                    response: "Bien jugado chico"
+                --
+                    user: "${input}"
+                    response:`,
+        max_tokens: 40,
+        temperature: 0.3,
+        k: 0,
+        p: 1,
+        frequency_penalty: 0,
+        stop_sequences:['--'],
+        return_likelihoods: 'NONE'
+    }
+
+    const response = await fetch(COHERE_API_URL,{
+        method: 'POST',
+        headers: {
+            Authorization: `BEARER ${COHERE_API_KEY}`,
+            "Content-Type": 'application/json',
+            "Cohere-Version": '2022-12-06'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+
+    return response.generations[0].text.split('"')[1]
+}
